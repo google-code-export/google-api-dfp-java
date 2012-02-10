21ee780988881e7e38bc35054ff67e6ddbcaf054
api.arogal@gmail.com <api.arogal@gmail.com@e5600b00-1bfd-11df-acd4-e1cde50d4098>
2012-02-10 21:40:05 +0000

    Additional files to sync

---
+++ examples/v201201/creativeservice/UpdateCreativesExample.java
@@ -0,0 +1,91 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v201201.creativeservice;
+
+import com.google.api.ads.dfp.lib.DfpService;
+import com.google.api.ads.dfp.lib.DfpServiceLogger;
+import com.google.api.ads.dfp.lib.DfpUser;
+import com.google.api.ads.dfp.lib.utils.v201201.StatementBuilder;
+import com.google.api.ads.dfp.v201201.Creative;
+import com.google.api.ads.dfp.v201201.CreativePage;
+import com.google.api.ads.dfp.v201201.CreativeServiceInterface;
+import com.google.api.ads.dfp.v201201.ImageCreative;
+import com.google.api.ads.dfp.v201201.Statement;
+
+/**
+ * This example updates the destination URL of all image creatives up to
+ * the first 500. To determine which image creatives exist, run
+ * GetAllCreativesExample.java.
+ *
+ * Tags: CreativeService.getCreativesByStatement
+ * Tags: CreativeService.updateCreatives
+ *
+ * @author api.arogal@gmail.com (Adam Rogal)
+ */
+public class UpdateCreativesExample {
+  public static void main(String[] args) {
+    try {
+      // Log SOAP XML request and response.
+      DfpServiceLogger.log();
+
+      // Get DfpUser from "~/dfp.properties".
+      DfpUser user = new DfpUser();
+
+      // Get the CreativeService.
+      CreativeServiceInterface creativeService =
+          user.getService(DfpService.V201201.CREATIVE_SERVICE);
+
+      // Create a statement to get all image creatives.
+      Statement filterStatement =
+          new StatementBuilder("WHERE creativeType = :creativeType LIMIT 500")
+              .putValue("creativeType", ImageCreative.class.getSimpleName()).toStatement();
+
+      // Get creatives by statement.
+      CreativePage page = creativeService.getCreativesByStatement(filterStatement);
+
+      if (page.getResults() != null) {
+        Creative[] creatives = page.getResults();
+
+        // Update each local creative object by changing its destination URL.
+        for (Creative creative : creatives) {
+          if (creative instanceof ImageCreative) {
+            ImageCreative imageCreative = (ImageCreative) creative;
+            imageCreative.setDestinationUrl("http://news.google.com");
+          }
+        }
+
+        // Update the creatives on the server.
+        creatives = creativeService.updateCreatives(creatives);
+
+        if (creatives != null) {
+          for (Creative creative : creatives) {
+            if (creative instanceof ImageCreative) {
+              ImageCreative imageCreative = (ImageCreative) creative;
+              System.out.println("An image creative with ID \"" + imageCreative.getId()
+                  + "\" and destination URL \"" + imageCreative.getDestinationUrl()
+                  + "\" was updated.");
+            }
+          }
+        } else {
+          System.out.println("No creatives updated.");
+        }
+      } else {
+        System.out.println("No creatives found to update.");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ examples/v201201/labelservice/GetLabelsByStatementExample.java
@@ -0,0 +1,73 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v201201.labelservice;
+
+import com.google.api.ads.dfp.lib.DfpService;
+import com.google.api.ads.dfp.lib.DfpServiceLogger;
+import com.google.api.ads.dfp.lib.DfpUser;
+import com.google.api.ads.dfp.lib.utils.v201201.StatementBuilder;
+import com.google.api.ads.dfp.v201201.Label;
+import com.google.api.ads.dfp.v201201.LabelPage;
+import com.google.api.ads.dfp.v201201.LabelServiceInterface;
+import com.google.api.ads.dfp.v201201.LabelType;
+import com.google.api.ads.dfp.v201201.Statement;
+
+/**
+ * This example gets all labels that are competitively excluded. The statement
+ * retrieves up to the maximum page size limit of 500. To create labels, run
+ * CreateLabelsExample.java. This feature is only available to DFP premium
+ * solution networks.
+ *
+ * Tags: LabelService.getLabelsByStatement
+ *
+ * @author api.arogal@gmail.com (Adam Rogal)
+ */
+public class GetLabelsByStatementExample {
+  public static void main(String[] args) {
+    try {
+      // Log SOAP XML request and response.
+      DfpServiceLogger.log();
+
+      // Get DfpUser from "~/dfp.properties".
+      DfpUser user = new DfpUser();
+
+      // Get the LabelService.
+      LabelServiceInterface labelService = user.getService(DfpService.V201201.LABEL_SERVICE);
+
+      // Create a statement to only select labels that are competitive
+      // sorted by name.
+      Statement filterStatement =
+          new StatementBuilder("WHERE type = :type ORDER BY name LIMIT 500")
+              .putValue("type", LabelType.COMPETITIVE_EXCLUSION.toString()).toStatement();
+
+      // Get labels by statement.
+      LabelPage page = labelService.getLabelsByStatement(filterStatement);
+
+      if (page.getResults() != null) {
+        int i = page.getStartIndex();
+        for (Label label : page.getResults()) {
+          System.out.println(i + ") Label with ID \"" + label.getId()
+              + "\", name \"" + label.getName()
+              + "\", and type \"" + label.getType() + "\" was found.");
+          i++;
+        }
+      }
+
+      System.out.println("Number of results found: " + page.getTotalResultSetSize());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
